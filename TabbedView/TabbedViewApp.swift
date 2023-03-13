170c23d589eeb48bf848178c4a7ca86e340e4c1a
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-03-13 13:34:11 -0300

    Initial Commit

---
+++ TabbedView/TabbedViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbedViewApp.swift
+//  TabbedView
+//
+//  Created by Jonn Alves on 13/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TabbedViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
